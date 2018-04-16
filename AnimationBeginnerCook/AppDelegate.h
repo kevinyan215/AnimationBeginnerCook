9157426fa0dd2d7f36b7ef7cbe5689a58d38858b
Kevin Yan <kevinyan215@gmail.com>
2018-04-15 22:38:36 -0400

    Initial Commit

---
+++ AnimationBeginnerCook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimationBeginnerCook
+//
+//  Created by Kevin Yan on 4/15/18.
+//  Copyright © 2018 Kevin Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
